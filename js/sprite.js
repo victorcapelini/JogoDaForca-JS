d65394c9da8f080f6cd95e651f95e93aee2a79e1
Victor <victorcapelini@hotmail.com>
2018-07-30 16:44:04 -0300

    Implementado a sprite da forca.
    Signed-off-by: Victor <victorcapelini@hotmail.com>

---
+++ js/sprite.js
@@ -0,0 +1,45 @@
+function createSprite(selector) {
+
+    var moveFrame = function (from, to) {
+        $el.removeClass(from)
+            .addClass(to);
+    };
+
+    var hasNext = function () {
+        return current + 1 <= last;
+    };
+
+    var nextFrame = function () {
+        if (hasNext()) {
+            moveFrame(frames[current], frames[++current]);
+        }
+    };
+
+    var isFinish = function () {
+        return !hasNext();
+    };
+
+    var reset = function () {
+        moveFrame(frames[current], frames[0]);
+        current = 0;
+    };
+
+    var $el = $(selector);
+
+    var frames = [
+        'frame1', 'frame2', 'frame3', 'frame4', 'frame5',
+        'frame6', 'frame7', 'frame8', 'frame9'
+    ];
+
+    var current = 0;
+
+    var last = frames.length - 1;
+
+    $el.addClass(frames[current]);
+
+    return {
+        nextFrame: nextFrame,
+        isFinish: isFinish,
+        reset: reset
+    };
+}
